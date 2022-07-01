d112f4d1eff8fd196dc9cf0558fea7d685e60a97
Anillag <101154249+anillag@users.noreply.github.com>
2022-06-30 22:19:38 -0400

    Mid-attempt at writing /api/notes POST

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require('path');
+const fs = require("fs");
 const app = express();
 const db = require("./db/db.json");
 const PORT = 3001;
+const { dbdata } = require("./db/db.json");
 
 app.use(express.static("public"));
 
@@ -14,12 +16,17 @@ app.get("/api/notes", (req, res) => {
   res.json(db);
 });
 
+app.post("/api/notes", (req, res) => {
+    console.log(res.json(req.body));
+    }
+);
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+    console.log(`Example app listening on port ${PORT}`);
 });
 
 // The application should have a db.json file on the back end
@@ -28,8 +35,6 @@ app.listen(PORT, () => {
 // The following HTML routes should be created:
 
 // The following API routes should be created:
-
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
 
 // POST /api/notes should receive a new note to save on the request body,
 // add it to the db.json file, and then return the new note to the client.
