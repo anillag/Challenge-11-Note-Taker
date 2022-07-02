85f1e52d828f76ea432e686d078776bb7fbefe2d
Anillag <101154249+anillag@users.noreply.github.com>
2022-07-02 17:48:23 -0400

    Updated fs logic to read/write/rewrite

--- server.js
+++ server.js
@@ -1,42 +1,81 @@
 const express = require("express");
 const path = require('path');
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
-const db = require("./db/db.json");
-const PORT = 3001;
-const { dbdata } = require("./db/db.json");
+const PORT = process.env.PORT || 3001;
 
+// Middleware for Express functionality
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// translates /notes to /public/notes.html
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
+// serves up db.json upon HTTP GET to /api/notes
 app.get("/api/notes", (req, res) => {
-  res.json(db);
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(JSON.parse(data));
+        };
+    });
 });
 
+// reads, adds, and rewrites db.json on HTTP POST
 app.post("/api/notes", (req, res) => {
-    console.log(res.json(req.body));
-    }
-);
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let notes = JSON.parse(data) || [];
+            let noteID = uuidv4();
+            const newNote = req.body;
+            console.log(noteID);
+            notes.push({...newNote, id: noteID});
+            fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json("Successfully added note!");
+                    console.log("Successfully added note!");
+                }
+            });
+        };
+    });
+});
 
+// deletes individual notes based on UUID-generated ID #
+app.delete("/api/notes/:id", (req, res) =>{
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let notes = JSON.parse(data) || [];
+            const id = req.params.id;
+            let deletedNote = notes.filter(note => (note.id !== id));
+            fs.writeFile("./db/db.json", JSON.stringify(deletedNote), (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json("Successfully deleted note!");
+                    console.log("Successfully deleted note!");
+                }
+            })
+        };
+    });
+});
+
+// catch-all to redirect to index.html
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// sets up Express to listen on PORT (see const's above)
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
-
-// The application should have a db.json file on the back end
-// that will be used to store and retrieve notes using the fs module.
-
-// The following HTML routes should be created:
-
-// The following API routes should be created:
-
-// POST /api/notes should receive a new note to save on the request body,
-// add it to the db.json file, and then return the new note to the client.
-// You'll need to find a way to give each note a unique id when it's saved
-// (look into npm packages that could do this for you).
